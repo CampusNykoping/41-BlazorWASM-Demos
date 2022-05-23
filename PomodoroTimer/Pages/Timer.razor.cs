dca87d6ed49c726202aeec2cef7e1761c5d8c44a
Claes Engelin <claes.engelin@zocom.se>
2022-05-23 13:18:21 +0200

    Add project files.

---
+++ PomodoroTimer/Pages/Timer.razor.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace PomodoroTimer.Pages;
+
+public class TimerBase : ComponentBase, IDisposable
+{
+    private int timeLeft = 25 * 60; // Tid i sekunder
+    protected string remaining => TimeSpan.FromSeconds(timeLeft).ToString(@"mm\:ss");
+    PeriodicTimer? timer;
+
+    protected async Task Start()
+    {
+        timer?.Dispose();
+        timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+        while (await timer.WaitForNextTickAsync())
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+    }
+
+    protected void Stop()
+    {
+        timer?.Dispose();
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+    }
+}
