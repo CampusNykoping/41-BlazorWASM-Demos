8bc173104d8267069aef491d975aa07a95394d84
Claes Engelin <claes.engelin@zocom.se>
2022-05-24 14:28:44 +0200

    Gjort klart demo 4, Shopping Cart

---
+++ ShoppingCartWithBackend/Client/Features/Cart/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using ShoppingCartStarter.Shared.Cart;
+
+using System.Net.Http.Json;
+
+namespace ShoppingCartStarter.Client.Features.Cart
+{
+    public partial class Index
+    {
+        [Inject] private HttpClient Http { get; set; }
+
+        protected Details.Model Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ReloadCart();
+        }
+
+        protected void RemoveItem(Details.Model.LineItem item)
+        {
+            Model.Items.Remove(item);
+        }
+
+        protected async Task ReloadCart()
+        {
+            Model = await Http.GetFromJsonAsync<Details.Model>("api/cart");
+        }
+    }
+}
