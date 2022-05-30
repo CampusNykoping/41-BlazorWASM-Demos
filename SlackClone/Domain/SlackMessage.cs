6d53b9ec8290e6b7b2ba225f306fb2330abb74d3
Claes Engelin <claes.engelin@zocom.se>
2022-05-30 14:02:32 +0200

    Halvvägs igenom demo 7, SlackClone

---
+++ SlackClone/Domain/SlackMessage.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SlackClone.Domain;
+public class SlackMessage
+{
+    public string PostedBy { get; set; }
+    public DateTime PostedOn { get; set; }
+    public string Message { get; set; }
+}
