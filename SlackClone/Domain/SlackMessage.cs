f8201151072a27f0ca58b90039352f98b3833ee7
Claes Engelin <claes.engelin@zocom.se>
2022-05-31 08:07:03 +0200

    Fixed SlackClone to build and work properly

--- SlackClone/Domain/SlackMessage.cs
+++ SlackClone/Domain/SlackMessage.cs
@@ -1,8 +1,11 @@
-﻿
-namespace SlackClone.Domain;
-public class SlackMessage
+﻿using System;
+
+namespace SlackClone.Domain
 {
-    public string PostedBy { get; set; }
-    public DateTime PostedOn { get; set; }
-    public string Message { get; set; }
+    public class SlackMessage
+    {
+        public string PostedBy { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Message { get; set; }
+    }
 }
